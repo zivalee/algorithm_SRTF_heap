bbfd9c227d26c21249450fe8a5a70fbc307c9e91
Ziva <zivalee87@gmail.com>
2020-10-07 20:59:31 +0800

    update comments

--- main.cpp
+++ main.cpp
@@ -14,48 +14,56 @@ using namespace std;
 // second: Arrival Time (rj)
 vector<int> findWaitingTime(vector<pair<int, int>> proc, int n, vector<int> wt) {
 
-    int r0 = 0, diff = 0, afterCountT = 0;
+    int diff = 0, afterCountT = 0;
     vector<int> heap;
     // put Process Length of first element into heap
     heap.push_back(proc[0].first);
     // make min heap
     make_heap(heap.begin(), heap.end(), greater<>{});
 
+    // adding process length of elements into heap
+    // examine every element's arrival time and adjust the heap before putting the process length into heap
     for (int i = 1; i < n; i++) {
+        // arrival time is in increasing order
+        // if arrival time of job i is equal to i-1, put the process length of it into heap directly
+        // if not, adjust heap then put the length into heap
         if (proc[i].second > proc[i - 1].second) {
+            // time passed between two job
             diff = proc[i].second - proc[i - 1].second;
-//            cout << diff << ", ";
 
-            // root min > diff
+            // if the time passed can't even finished one job
+            // root of heap (min job length) > time passed
             if (heap.front() > diff) {
                 heap.front() -= diff;
             } else {
-                // end time
+                // if the time passed can finish more than one job
+                // arrival time of the previous job
                 int previousT = proc[i - 1].second;
 
                 while (diff > 0) {
                     int temp = heap.front();
 
                     if (diff >= temp) {
+                        // if can finish more jobs
                         // delete root
                         pop_heap(heap.begin(), heap.end(), greater<>{});
                         heap.pop_back();
-                        // diff
+                        // update time passed (diff)
                         diff -= temp;
-                        // ?
+                        // count job finished time
                         wt.push_back(temp + previousT);
+                        // update previous job finished time
                         previousT = temp + previousT;
                     } else {
+                        // if no time left
                         heap.front() -= diff;
                         diff = 0;
                     }
                 }
-
             }
-
         }
 
-        // insert to heap
+        // insert job length to heap
         heap.push_back(proc[i].first);
         push_heap(heap.begin(), heap.end(), greater<>{});
 
@@ -73,12 +81,14 @@ vector<int> findWaitingTime(vector<pair<int, int>> proc, int n, vector<int> wt) 
 
 //    cout << "afterCountT "<< afterCountT << endl;
 
+    // after all process length are put into heap
+    // count the finished time of the remaining jobs in heap
     int num =heap.size();
     for (int j = 0; j < num; j++) {
         // count time
         wt.push_back(afterCountT + heap.front());
         afterCountT += heap.front();
-        // delete root
+        // delete the counted root min
         pop_heap(heap.begin(), heap.end(), greater<>{});
         heap.pop_back();
     }
@@ -154,23 +164,23 @@ int main() {
 //            cout << data[j].first << ",  ";
 //        }
 
-        clock_t start = clock();
         // SRPT
+        clock_t start = clock();
         vector<int> wt;
         wt = findWaitingTime(process, process.size(), wt);
-
         clock_t end = clock();
+
+        // runtime
         double elapsed_secs = double(end - start);
         cout << "elapsed run time: " << elapsed_secs << " ms, ";
 
+        // count process finished time
         int total = 0;
-        // print process finished time
         for (int b = 0; b < wt.size(); b++) {
             total += wt[b];
         }
         cout << "Objective Value: " << total << endl;
     }
-
 
     return 0;
 }
